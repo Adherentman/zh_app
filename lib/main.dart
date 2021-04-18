5781a08534429af77b92efa9fdb99ffc3a5471a1
徐梓昊 <adherentxu@gmail.com>
2021-04-18 17:22:40 +0800

    feat: add bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:zhapp/widget/setting/themePage.dart';
@@ -8,7 +9,8 @@ import 'navigationBar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
   // 透明状态栏
   if (Platform.isAndroid) {
     final SystemUiOverlayStyle systemUiOverlayStyle =
@@ -18,17 +20,33 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({this.savedThemeMode});
+  final AdaptiveThemeMode? savedThemeMode;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => NavigationBar(),
-        'setting/theme_page': (context) => ThemePage()
-      },
-    );
+    return AdaptiveTheme(
+        light: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.red,
+          accentColor: Colors.amber,
+        ),
+        dark: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.red,
+          accentColor: Colors.amber,
+        ),
+        initial: savedThemeMode ?? AdaptiveThemeMode.system,
+        builder: (theme, darkTheme) => MaterialApp(
+              title: 'Flutter Demo',
+              theme: theme,
+              darkTheme: darkTheme,
+              debugShowCheckedModeBanner: false,
+              initialRoute: '/',
+              routes: {
+                '/': (context) => NavigationBar(),
+                'setting/theme_page': (context) => ThemePage()
+              },
+            ));
   }
 }
--- lib/widget/setting/themePage.dart
+++ lib/widget/setting/themePage.dart
@@ -1,16 +1,24 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 
 class ThemePage extends StatelessWidget {
   final List<String> _list = ['跟随系统', '开启', '关闭'];
 
-  Future<void> setThemeMode(int index) async {
-    ThemeMode themeMode = index == 0
-        ? ThemeMode.system
-        : (index == 1 ? ThemeMode.dark : ThemeMode.light);
-    // SharedPreferencesDataUtils.set(
-    //     SharedPreferencesDataUtils.THEME_PREF, themeMode);
-    // themeManager.setThemeMode(themeMode);
-    // themeManager.setThemeModeItem(ThemeController.themeDict[themeMode.value]);
+  Future<void> setThemeMode(int index, BuildContext context) async {
+    switch (index) {
+      case 0:
+        AdaptiveTheme.of(context).setSystem();
+        break;
+      case 1:
+        AdaptiveTheme.of(context).setDark();
+        break;
+      case 2:
+        AdaptiveTheme.of(context).setLight();
+        break;
+      default:
+        AdaptiveTheme.of(context).setLight();
+        break;
+    }
   }
 
   @override
@@ -25,7 +33,7 @@ class ThemePage extends StatelessWidget {
           itemBuilder: (_, index) {
             return InkWell(
               onTap: () {
-                setThemeMode(index);
+                setThemeMode(index, context);
               },
               child: Container(
                 alignment: Alignment.centerLeft,
@@ -34,12 +42,10 @@ class ThemePage extends StatelessWidget {
                 child: Row(
                   children: <Widget>[
                     Expanded(child: Text(_list[index])),
-                    // Observer(
-                    //   builder: (_) => Opacity(
-                    //     opacity:
-                    //         themeManager.themeModeItem == _list[index] ? 1 : 0,
-                    //     child: Icon(Icons.done, color: Colors.blue),
-                    //   ),
+                    // Opacity(
+                    //   opacity:
+                    //       themeManager.themeModeItem == _list[index] ? 1 : 0,
+                    //   child: Icon(Icons.done, color: Colors.blue),
                     // ),
                   ],
                 ),
